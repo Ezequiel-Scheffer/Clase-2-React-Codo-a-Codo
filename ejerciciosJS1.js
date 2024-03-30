331b1950637659785f907501565eb0d7ac1d14fb
Ezequiel Scheffer <132506024+Ezequiel-Scheffer@users.noreply.github.com>
2024-03-30 14:20:27 -0300

    Update ejerciciosJS1.js
    Se agrego la funcion que faltaba esPositivo()

--- ejerciciosJS1.js
+++ ejerciciosJS1.js
@@ -161,13 +161,16 @@ console.log((Math.random()))
 
 //....................................
 
-function esPositivo(numero) {
-  //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
-  //Si el número es positivo, devolver ---> "Es positivo"
-  //Si el número es negativo, devolver ---> "Es negativo"
-  //Si el número es 0, devuelve false
+//La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
+//Si el número es positivo, devolver ---> "Es positivo"
+//Si el número es negativo, devolver ---> "Es negativo"
+//Si el número es 0, devuelve false
 
-}
+let esPositivo = (numero) => {
+  numero > 0 ? console.log("Es positivo"):
+  numero < 0 ? console.log("Es negativo"):
+  console.log("false")
+} 
 
 //....................................
 
